245093635018364fd59f80a4f84378320cd2de43
anduresu <andrescarias1@gmail.com>
2025-01-04 18:02:47 -0600

    model setting

--- yugioh_proyect/yugioh_app/admin.py
+++ yugioh_proyect/yugioh_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Carta, Deck, CartaDeck, Partida
 # Register your models here.
+
+@admin.register(Carta)
+class CartaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'tipo', 'nivel', 'ataque', 'defensa')
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'usuario', 'fecha_creacion')
+
+@admin.register(CartaDeck)
+class CartaDeckAdmin(admin.ModelAdmin):
+    list_display = ('deck', 'carta', 'cantidad')
+
+@admin.register(Partida)
+class PartidaAdmin(admin.ModelAdmin):
+    list_display = ('jugador1', 'jugador2', 'ganador', 'fecha_inicio', 'fecha_fin')
+
--- yugioh_proyect/yugioh_app/models.py
+++ yugioh_proyect/yugioh_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Carta(models.Model):
+    nombre = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255)
+    nivel = models.IntegerField()
+    ataque = models.IntegerField()
+    defensa = models.IntegerField()
+    descripcion = models.TextField()
+    imagen_url = models.URLField()
+    
+    def __str__(self):
+        return self.nombre
+
+# modelo para los decks
+class Deck(models.Model):
+    nombre = models.CharField(max_length=255)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.usuario.username}'
+
+# modelo realacion muchos a muchos entre deck y carta
+class CartaDeck(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    carta = models.ForeignKey(Carta, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.cantidad}x {self.carta.nombre} en {self.deck.nombre}'
+
+#model para las partidas
+class Partida(models.Model):
+    jugador1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='jugador1')
+    jugador2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='jugador2')
+    ganador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ganador')
+    fecha_inicio = models.DateTimeField(auto_now_add=True)
+    fecha_fin = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.jugador1.username} vs {self.jugador2.username}'
