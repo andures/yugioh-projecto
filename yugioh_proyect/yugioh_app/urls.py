378ba4f3152c16ff0b98eedfb869d369f8523780
anduresu <andrescarias1@gmail.com>
2025-01-04 21:42:47 -0600

    funciones y htmls agregados

---
+++ yugioh_proyect/yugioh_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('cartas/', views.cartas_list, name='cartas_list'),
+    path('decks/', views.decks_list, name='decks_list'),
+    path('decks/<int:deck_id>/', views.deck_detail, name='deck_detail'),
+    path('decks/create/', views.deck_create, name='deck_create'),
+    path('decks/create/<int:deck_id>/', views.deck_create, name='deck_create'),
+]
--- yugioh_proyect/yugioh_app/views.py
+++ yugioh_proyect/yugioh_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Carta, Deck, CartaDeck
 
-# Create your views here.
+# Vista para la página de inicio
+def home(request):
+    return render(request, 'yugioh_app/home.html')
+
+# Vista para listar todas las cartas
+def cartas_list(request):
+    cartas = Carta.objects.all()  # Obtener todas las cartas
+    return render(request, 'yugioh_app/cartas_list.html', {'cartas': cartas})
+
+# Vista para listar todos los decks
+def decks_list(request):
+    decks = Deck.objects.all()  # Obtener todos los decks
+    return render(request, 'yugioh_app/decks_list.html', {'decks': decks})
+
+# Vista para mostrar los detalles de un deck específico
+def deck_detail(request, deck_id):
+    deck = get_object_or_404(Deck, pk=deck_id)  # Obtener el deck por su ID
+    # Obtener todas las cartas asociadas a este deck
+    cartas_en_deck = CartaDeck.objects.filter(deck=deck)
+    return render(request, 'yugioh_app/deck_detail.html', {'deck': deck, 'cartas_en_deck': cartas_en_deck})
+
+# vista para crear un nuevo deck
+def deck_create(request, deck_id=None):
+    if deck_id:
+        #si el deck ya existe, se edita
+        deck = get_object_or_404(Deck, pk=deck_id, usuario=request.user)
+    else:
+        # si no existe, se crea uno nuevo 
+        deck = Deck.objects.create(nombre='Nuevo Deck', usuario=request.user)
+    
+    return render(request, 'yugioh_app/deck_create.html', {'deck': deck})
